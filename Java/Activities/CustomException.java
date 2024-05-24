3c7f0cbfed9aa0eac6e3c85208b16943ffd55b90
sanchari9 <Sanchari.Saha@ibm.com>
2024-05-24 20:09:49 +0530

    Add files via upload

---
+++ Java/Activities/CustomException.java
@@ -0,0 +1,12 @@
+package Activities;
+
+public class CustomException extends Exception{
+    private String message;
+    public CustomException(String message){
+        this.message = message;
+
+    }
+    public String getMessage() {
+        return message;
+    }
+}
