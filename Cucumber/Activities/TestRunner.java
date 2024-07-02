a69cf27da321dde2c096eac5ab72f2565698b127
sanchari9 <Sanchari.Saha@ibm.com>
2024-07-02 15:47:46 +0530

    Add files via upload

---
+++ Cucumber/Activities/TestRunner.java
@@ -0,0 +1,20 @@
+package testRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/Features",
+        glue = {"stepDefinitions"},
+        tags = "@Activity6",
+        publish = true,
+        plugin = {"pretty"},
+        monochrome = true)
+//        plugin = {"html:src/reports/HTML_Report.xml",
+//                "json:src/reports/JSON_Report.xml",
+//                "junit:src/reports/XML_Report.xml"
+//        }
+public class TestRunner {
+	//Empty
+}
